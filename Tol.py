40a318c40ac8d58725deb0bf69c7baa880339010
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-21 10:48:40 +0800

    Create Tol.py

---
+++ Tol.py
@@ -0,0 +1,11 @@
+
+users = {'omfar1': '1231', 'omfar2': '1232', 'omfar3'>
+
+def login():
+    username = input("MASUKAN NAMA ANDA : ")
+    password = input("MASUKAN PASSWORD ANDA : ")
+
+    if username in users and users[username] == passw>
+        print("SELAMAT DATADNG")
+    else:
+        print("NAMA DAN SANDI SALAH ")
